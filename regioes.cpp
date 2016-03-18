6099c8f7af07a96c115555ca94451a76d9fd8bcf
Jonathan Lima <jonathangabriel@bct.ect.ufrn.br>
2016-03-17 21:52:40 -0300

    Atualizacao 5

--- regioes.cpp
+++ regioes.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
 #include <cv.h>
 #include <highgui.h>
 	
@@ -7,8 +6,6 @@
 	using namespace std;
 
 int main(int argc, char** argv){
-
-	int P1i, P1j, P2i, P2j;
 
 	Mat image;
 
@@ -18,30 +15,14 @@ int main(int argc, char** argv){
 
 	namedWindow("janela",WINDOW_AUTOSIZE);
 
-	cout << "Digite P1 i" << endl;
-	cin >> P1i;
-	cout << "Digite P1 j" << endl;
-	cin >> P1j;
-
-	cout << "Digite P2 i" << endl;
-	cin >> P2i;
-	cout << "Digite P2 j" << endl;
-	cin >> P2j;
-
-	  Rect roi(P1i, P2i, P1j, P2j);
-
 	int val;
 
-	for(int i=0;i<image.rows;i++){
-		for(int j=0;j<image.cols;j++){
-			if(i >= P1i && i <= P2i && j >= P1j && j <= P2j ){
-				val = 255 - image.at<uchar>(i,j);
-			}else
-				val = image.at<uchar>(i,j);
-
-			image.at<uchar>(i,j) = val;
-		}
-	}
+	 for(int i=0;i<image.rows;i++)
+{    for(int j=0;j<image.cols;j++){
+      val = 255 - image.at<uchar>(i,j);
+      image.at<uchar>(i,j) = val;
+    }
+  }
 
   imshow("janela", image);  
   waitKey();
